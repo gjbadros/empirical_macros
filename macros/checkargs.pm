afeb995f5fc597af9be8a2f2a14649be74070a58
mernst <>
1997-01-11 01:51:31 +0000

    Make check_args* return the args; modify uses; more function argument declarator hacking; add accessors to type spec.

--- macros/checkargs.pm
+++ macros/checkargs.pm
@@ -9,17 +9,27 @@ use strict;
 
 =head1 NAME
 
-checkargs -- Package to provide rudimentary argument checking for perl5 functions
+checkargs -- Provide rudimentary argument checking for perl5 functions
 
 =head1 SYNOPSIS
 
-At the beginning of a user-written function, do:
+  check_args(cArgsExpected, szFunctionName, caller, @_)
+  check_args_range(cArgsMin, cArgsMax, szFunctionName, caller, @_)
+  check_args_at_least(cArgsMin, szFunctionName, caller, @_)
+where "caller" and "@_" should be supplied literally.
 
-C<check_args( cArgsExpected, szFunctionName, caller, @_)>
+As the first line of user-written subroutine foo, do one of the following:
 
-C<check_args_range( cArgsMin>, cArgsMax>, szFunctionName, caller, @_)>
+  my ($arg1, $arg2) = check_args(2, "foo", caller, @_);
+  my ($arg1, @rest) = check_args_range(1, 4, "foo", caller, @_);
+  my ($arg1, @rest) = check_args_at_least(1, "foo", caller, @_);
+  my @args = check_args_at_least(0, "foo", caller, @_);
 
-C<check_args_at_least( cArgsMin, szFunctionName, caller,@_)>
+These functions may also be called for side effect (put a call to one
+of the functions near the beginning of the subroutine), but using the
+argument checkers to set the argument list is the recommended usage.
+
+The number of arguments and their definedness are checked.
 
 =head1 DESCRIPTION
 
@@ -41,6 +51,9 @@ sub check_args
 {
   my ($num_formals, $function_name, $package, $filename, $line, @args) = @_;
   if (@_ < 5) { die "check_args needs at least 5 args, got: @_\n"; }
+  if ((!wantarray) && ($num_formals != 0))
+    { ($package, $filename, $line) = caller;
+      die "check_args called in scalar context by $function_name at $filename line $line: @args\n"; }
   my $num_actuals = @args;
   if ($num_actuals != $num_formals)
     { die "function $function_name expected $num_formals argument",
@@ -51,12 +64,16 @@ sub check_args
   for $index (0..$#args)
     { if (!defined($args[$index]))
 	{ die "function $function_name undefined argument ", $index+1, " at $filename line $line: @args\n"; } }
+  return @args;
 }
 
 sub check_args_range
 {
   my ($min_formals, $max_formals, $function_name, $package, $filename, $line, @args) = @_;
   if (@_ < 6) { die "check_args_range needs at least 6 args, got: @_\n"; }
+  if (!wantarray)
+    { ($package, $filename, $line) = caller;
+      die "check_args_range called in scalar context at $filename line $line: @args\n"; }
   my $num_actuals = @args;
   if (($num_actuals < $min_formals) || ($num_actuals > $max_formals))
     { die "function $function_name expected $min_formals-$max_formals arguments, got $num_actuals at $filename line $line",
@@ -65,14 +82,19 @@ sub check_args_range
   for $index (0..$#args)
     { if (!defined($args[$index]))
 	{ die "function $function_name undefined argument ", $index+1, " at $filename line $line: @args\n"; } }
+  return @args;
 }
 
 sub check_args_at_least
 {
   my ($min_formals, $function_name, $package, $filename, $line, @args) = @_;
-  if ($min_formals == 0)
-    { die "Isn't it pointless to check for at least zero args to $function_name?\n"; }
+  # Don't do this, because we want every sub to start with a call to check_args*
+  # if ($min_formals == 0)
+  #   { die "Isn't it pointless to check for at least zero args to $function_name?\n"; }
   if (@_ < 6) { die "check_args_at_least needs at least 6 args, got: @_\n"; }
+  if (!wantarray)
+    { ($package, $filename, $line) = caller;
+      die "check_args_at_least called in scalar context at $filename line $line: @args\n"; }
   my $num_actuals = @args;
   if ($num_actuals < $min_formals)
     { die "function $function_name expected at least $min_formals argument",
@@ -83,6 +105,7 @@ sub check_args_at_least
   for $index (0..$#args)
     { if (!defined($args[$index]))
 	{ die "function $function_name undefined argument ", $index+1, " at $filename line $line: @args\n"; } }
+  return @args;
 }
 
 1;				# successful import
