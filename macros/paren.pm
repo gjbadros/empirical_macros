4e2fdfa87a76ac7cfafe3ddd2f7fd37442262d09
mernst <>
1997-08-02 16:51:46 +0000

    Rename $debug_find_paren to $debug_paren Use /g in while (m//) tests New boolean function contains_mismatch

--- macros/paren.pm
+++ macros/paren.pm
@@ -4,7 +4,7 @@ package paren;
 require 5.000;
 require Exporter;
 @ISA = qw(Exporter);
-@EXPORT = qw(brace_change paren_change find_close_paren find_close_brace);
+@EXPORT = qw(brace_change paren_change contains_mismatch find_close_paren find_close_brace);
 use checkargs;
 use strict;
 use Carp;
@@ -18,6 +18,7 @@ paren -- Find balanced parentheses and braces
 
   brace_change (line)
   paren_change (line)
+  contains_mismatch (line)
   find_close_paren (line)
   find_close_paren (line, startpos)
   find_close_brace (line)
@@ -42,30 +43,31 @@ Michael D. Ernst <F<mernst@cs.washington.edu>>
 my $true = (1 == 1);
 my $false = (1 == 0);
 
-my $debug_find_paren = $false;
-# $debug_find_paren = $true;
+my $debug_paren = $false;
+# $debug_paren = $true;
 
 
 ###########################################################################
 ### Counting change
 ###
 
+# These should also check whether result_num ever becomes negative.
+
 # Takes a simplified line as its argument; returns a string containing the
 # unbalanced braces (empty string if all braces are balanced).
 sub brace_change ($)
 { my ($line) = check_args(1, @_);
-  if ($debug_find_paren) { print "brace_change $line"; }
+  if ($debug_paren) { print "brace_change $line"; }
   my $result_num = 0;
   my $result_chars = "";
-  while ($line =~ /[\{\}]/)
+  while ($line =~ /[\{\}]/g)
     { $result_chars .= $MATCH;
       if ($MATCH eq "\{")
 	{ $result_num++; }
       elsif ($MATCH eq "\}")
 	{ $result_num--; }
       else
-	{ die("What match? $MATCH"); }
-      $line = $POSTMATCH; }
+	{ die("What match? $MATCH"); } }
   return ($result_num == 0) ? $false : $result_chars;
 }
 
@@ -75,19 +77,57 @@ sub brace_change ($)
 # found before parens balance).
 sub paren_change ($)
 { my ($line) = check_args(1, @_);
-  if ($debug_find_paren) { print "paren_change $line"; }
+  if ($debug_paren) { print "paren_change $line"; }
   my $result_num = 0;
   my $result_chars = "";
-  while ($line =~ /[\(\)]/)
+  while ($line =~ /[\(\)]/g)
     { $result_chars .= $MATCH;
       if ($MATCH eq "\(")
 	{ $result_num++; }
       elsif ($MATCH eq "\)")
 	{ $result_num--; }
       else
-	{ die("What match? $MATCH"); }
-      $line = $POSTMATCH; }
+	{ die("What match? $MATCH"); } }
   return ($result_num == 0) ? $false : $result_chars;
+}
+
+
+# Returns a boolean: $true if a mismatch (close without open, or open
+# without close, or mismatched open and close), $false otherwise
+
+# Ignores strings, character constants, comments.
+# Largely lifted from old check_macro_body (which didn't ignore those, and
+# did more to boot, but also tried to do too much).
+sub contains_mismatch ($)
+{ my ($body) = check_args(1, @_);
+  if ($debug_paren) { print "contains_mismatch <= $body"; }
+
+  my $result = 0;
+  my @nesting = ();
+
+  my $full_body = $body;
+
+  while ($body =~ m/[\]\[(){}]/go) #'HACKCOLOR
+    # Found paren, quote, cpp directive, or comment
+    { my $match = $MATCH;
+      if ($debug_paren) { print "contains_mismatch matched '$match'\n" }
+      if ($match =~ /[\[\(\{]/)
+        { push(@nesting,$match);
+	  # print STDERR "FRONTMATCH\n";
+	}
+      else # ($match =~ /[\]\)\}]/)
+	{ if (scalar(@nesting) == 0)
+	    { return $true; }
+	  else
+	    { my $popped = pop(@nesting);
+	      if ((($match eq "\]") && ($popped ne "\["))
+		  || (($match eq "\)") && ($popped ne "\("))
+		  || (($match eq "\}") && ($popped ne "\{")))
+		{ return $true; } } } }
+  if (scalar(@nesting) != 0)
+    { return $true; }
+
+  return $false;
 }
 
 
@@ -105,7 +145,7 @@ sub find_close_paren ($;$)
 { my ($exp, $pos) = check_args_range(1, 2, @_);
   if (!defined($pos))
     { $pos = 0; }
-  if ($debug_find_paren) { print "find_close_paren: $exp\n"; }
+  if ($debug_paren) { print "find_close_paren: $exp\n"; }
   my $opens = 1;
   my $brace_pos;			# undefined if no brace found
   { my $open_pos = index($exp, "\{", $pos);
@@ -120,7 +160,7 @@ sub find_close_paren ($;$)
   while ($opens > 0)
     { my $next_open = index($exp, "\(", $pos);
       my $next_close = index($exp, "\)", $pos);
-      if ($debug_find_paren)
+      if ($debug_paren)
 	{ print "next_open $next_open next_close $next_close\n"; }
       if (($next_close != -1)
 	  && (($next_open == -1) || ($next_close < $next_open)))
@@ -138,7 +178,7 @@ sub find_close_paren ($;$)
 	      return $false; }
 	  $opens++;
 	  $pos = $next_open+1;
-	  if ($debug_find_paren)
+	  if ($debug_paren)
 	    { print "open paren at $next_open, opens = $opens, pos = $pos\n"; }
 	}
       else
@@ -147,26 +187,26 @@ sub find_close_paren ($;$)
 	    { die "bad values for next_open $next_open and next_close $next_close"; }
 	  return $false; }
     }
-  if ($debug_find_paren)
+  if ($debug_paren)
     { print "find_close_paren(\"$exp\") => ", $pos-1, "\n"; }
   return $pos - 1;
 }
 
 # Return index of first close brace which doesn't match a preceding open brace
-# in the argument string, and the argument string.  Ignores strings, parens, etc.
+# in the argument string.  Ignores strings, parens, etc.
 # Optional second argument says where to start (right after the open, usually).
 sub find_close_brace ($;$)
 { my ($exp, $pos_init) = check_args_range(1, 2, @_);
   if (!defined($pos_init))
     { $pos_init = 0; }
-  if ($debug_find_paren) { print "find_close_brace: $exp\n"; }
+  if ($debug_paren) { print "find_close_brace: $exp\n"; }
   my $opens = 1;
   my $pos = $pos_init;
 
   while ($opens > 0)
     { my $next_open = index($exp, "\{", $pos);
       my $next_close = index($exp, "\}", $pos);
-      if ($debug_find_paren)
+      if ($debug_paren)
 	{ print "next_open $next_open next_close $next_close\n"; }
       if (($next_close != -1)
 	  && (($next_open == -1) || ($next_close < $next_open)))
@@ -176,7 +216,7 @@ sub find_close_brace ($;$)
 	     && (($next_close == -1) || ($next_open < $next_close)))
 	{ $opens++;
 	  $pos = $next_open+1;
-	  if ($debug_find_paren)
+	  if ($debug_paren)
 	    { print "open brace at $next_open, opens = $opens, pos = $pos\n"; }
 	}
       else
