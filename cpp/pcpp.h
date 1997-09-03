6fe4b0212776cdb0891ff65319bd3cb3807a187f
gjb <>
1997-09-03 19:45:22 +0000

    Added

---
+++ cpp/pcpp.h
@@ -0,0 +1,18 @@
+/* $Id$
+ * By Greg J. Badros -- Aug. 28th, 1997
+ */
+
+#ifndef _CPPMAIN_H_
+#define _CPPMAIN_H_
+
+#include "cpplib.h"
+#include "cpphook.h"
+
+extern cpp_reader parse_in;
+extern cpp_options options;
+
+char * SzFromToken(enum cpp_token kind);
+
+extern int yydebug;
+
+#endif _CPPMAIN_H_
