220e4f890f7714a2a0caa6f1a55c188ff1c9a2cc
gjb <>
1997-10-09 01:04:54 +0000

    Added

---
+++ cpp/test5.h
@@ -0,0 +1,17 @@
+#if !defined(FOO)
+Next line here
+#elif defined(BAR)
+Another set of lines that 
+all get skipped over
+#else
+and this goes out
+#endif
+
+
+#ifdef FOO
+FOO defined
+#elif defined(BAR)
+BAR defined
+#else
+Neither defined
+#endif
