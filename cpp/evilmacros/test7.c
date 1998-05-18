37525a6d1c78c6292f4399ae140d7c465d215759
gjb <>
1998-05-18 18:30:36 +0000

    Added

---
+++ cpp/evilmacros/test7.c
@@ -0,0 +1,8 @@
+#define const __const
+#define double float
+
+const int a = 5;
+#ifdef FOO
+const int b = 6;
+#endif
+const double c = 2.0;
---
+++ cpp/evilmacros/test8.c
@@ -0,0 +1,8 @@
+#define const __const
+#define double float
+
+const int a = 5;
+#if FOO != 0
+const int b = 6;
+#endif
+const double c = 2.0;
